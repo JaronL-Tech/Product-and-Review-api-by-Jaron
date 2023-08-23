1e7f983894625bf730eaa6f039f50cd7b5ac0108
Jaron <Jaronleach98@gmail.com>
2023-08-23 14:20:40 -0400

    Uploading starting code.

---
+++ DTO/ReviewDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductandReviewAPI.DTO
+{
+    public class ReviewDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Rating { get; set; }
+    }
+}
