dea3520baefcd275bbe42b12861c3a5f9cf87202
Jaron Leach <jaronleach98@gmail.com>
2023-10-24 15:47:50 -0400

    Worked on fixing controllers and adding postman json file to project.Which can be located in the repo.

--- DTO/ReviewDTO.cs
+++ DTO/ReviewDTO.cs
@@ -9,14 +9,6 @@ namespace ProductandReviewAPI.DTO
         public string Text { get; set; }
         public int Rating { get; set; }
     }
-    public class ProductDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-        public ICollection<ReviewDTO> Reviews  { get; set; }
-        public double AverageRating { get; set; }
-
-    }
+    
 
 }
